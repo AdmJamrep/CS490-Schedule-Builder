5a2a79bca49e686f1b9afe3705696408d9e5ff5a
Brian Corzo <bc55@njit.edu>
2012-02-08 19:46:00 -0500

    Login form and output php file added.

---
+++ application/views/login_form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>CS 490 Project -- Alpha</title>
+</head>
+
+<body>
+
+<!-- User enters name in this form and submits -->
+<form action = "index.php/login/login" method = "post" />
+<p>Enter your name: <input type = "text" name="username" /></p>
+<input type = "submit" value = "Submit" />
+</form>
+
+
+</body>
+</html>
